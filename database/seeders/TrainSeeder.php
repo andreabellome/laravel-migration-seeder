4cf30d83e61c991b203ed114e648107e146bf673
Andrea Bellome <andrea.bellome@gmail.com>
2023-06-26 16:46:13 +0200

    debug

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -23,12 +23,12 @@ class TrainSeeder extends Seeder
 
             $train->company = $faker->company();
             $train->departure_station = $faker->randomElement(['Roma', 'Milano', 'Napoli']);
-            $train->arrival_station = $faker->randomElemen(['Palermo', 'Rimini', 'Genova']);
+            $train->arrival_station = $faker->randomElement(['Palermo', 'Rimini', 'Genova']);
             $train->departure_time = $faker->dateTimeBetween('-1 day','+1 day');
             $train->arrival_time = $faker->dateTimeBetween('-2 day','+2 day');
             $train->train_code = 'AB'.$faker->randomDigit();
-            $train->train_code = $faker->randomDigit();
-            $train->on_time = $faker->boolean(70);
+            $train->wagons_number = $faker->randomDigit();
+            $train->ontime = $faker->boolean(70);
             $train->cancelled = $faker->randomDigit(20);
 
             $train->save();
